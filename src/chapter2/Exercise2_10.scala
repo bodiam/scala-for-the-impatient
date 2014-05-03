af1006aa31c22f021c28ee25995980d1277ecc6d
Erik Pragt <erik.pragt@jworks.nl>
2014-05-03 22:10:42 +0200

    Added last exercise.

--- src/chapter2/Exercise2_10.scala
+++ src/chapter2/Exercise2_10.scala
@@ -5,4 +5,16 @@ package chapter2
  */
 object Exercise2_10 {
 
+
+  def main(args: Array[String]) {
+    println(math.pow(2, 2), pow2(2, 2))
+    println(math.pow(2, -2), pow2(2, -2))
+  }
+
+  def pow2(x: Double, n: Int): Double = {
+    if (n > 0 && n % 2 == 0) pow2(x, n / 2) * pow2(x, n / 2)
+    else if (n > 0) x * pow2(x, n - 1)
+    else if (n == 0) 1
+    else 1 / pow2(x, -n)
+  }
 }
